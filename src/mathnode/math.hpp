552c1508623bd79d0885e80f807aa98a6398b39b
Vkpandiripally <69334550+Vkpandiripally@users.noreply.github.com>
2023-11-19 20:53:11 -0600

    added stuff for math.hpp

--- src/mathnode/math.hpp
+++ src/mathnode/math.hpp
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #ifndef MATH_NODE_H_ 
+#define MATH_NODE_H_
+
+#include "operatorNode.h"
+#include "numNode.h"
+#include <vector>
 
 class MathNode{
     
@@ -9,6 +14,21 @@ class MathNode{
     // Works with operator and num node
     // Will assign to respective nodes
     public:
+
+        void createNumNode(double value){
+            NumNode* num = new numNode(value);
+            numNode.push_back(num);
+        }
+
+        //is_num == bool
+        bool is_number() const{
+            for (const auto&num : numNode){
+                if (num -> is_()){
+                    return true;
+                }
+            }
+            return false;
+        }
 
     
     //protected
@@ -22,7 +42,13 @@ class MathNode{
     
     
     //constructor
-    MathNode()
+    //delete allocated memory
+    ~MathNode(){
+        delete operatorNode;
+        for (auto num : numNode)}{
+            delete : num;
+        }
+    }
     
     
     //Needs to handle every integer in user input
